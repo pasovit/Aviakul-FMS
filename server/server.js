8c4d76dd44dcedf08a7c6641fb7b5cb8b8422757
raveenkarri <raveenkarri@gmail.com>
2026-01-12 17:14:57 +0530

    Updated small error

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config(); // MUST be first
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -8,8 +10,8 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const rateLimit = require("express-rate-limit");
 
-// Load environment variables
-dotenv.config();
+// //Load environment variables
+// dotenv.config();
 
 // Import routes
 const authRoutes = require("./routes/authRoutes");
@@ -114,6 +116,8 @@ app.use(errorHandler);
 // Database connection
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI =", process.env.MONGO_URI);
+
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
